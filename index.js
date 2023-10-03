8f25129638d0be43f42b5477aad53355983d5803
Nishanth195 <144122808+Nishanth195@users.noreply.github.com>
2023-10-03 11:28:01 +0530

    Update index.js
    final

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import { MongoClient,ObjectId } from 'mongodb';
+import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import moment from 'moment-timezone';
 
-const uri = 'mongodb://localhost:27017/trial'; // Your MongoDB URI
-const client = new MongoClient(uri, {  });
+const uri = 'mongodb://localhost:27017/trial'; // MongoDB URI
+const client = new MongoClient(uri, {});
+
+// Set your desired time zone here
+moment.tz.setDefault('Asia/India'); //
 
 async function connectToMongoDB() {
   try {
     await client.connect();
     console.log('MongoDB connected successfully');
-    const db = client.db('trial'); // Replace with your actual database name
+    const db = client.db('trial'); //database name
 
     // CRUD operations and aggregation can access the 'db' object here
     await performCRUDOperations(db);
@@ -24,32 +29,33 @@ async function connectToMongoDB() {
 }
 
 async function addTestPosts(db) {
-    const postsCollection = db.collection('posts');
-    
-    try {
-      const now = new Date(); // Current date and time
-      const lastWeek = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000); // 7 days ago
-  
-      const testPosts = [
-        { groupId:new ObjectId("6516eadbb029d404b0c196a4"), createdAt: lastWeek },
-        { groupId:new ObjectId("6516d690f084a3a3da0cf28b"), createdAt: lastWeek },
-        { groupId:new ObjectId("6517d51dcbde365d3bfd0b02"), createdAt: lastWeek },
-        // Add more test posts with different group IDs and recent timestamps
-      ];
-  
-      await postsCollection.insertMany(testPosts);
-      console.log('Test posts added:', testPosts);
-    } catch (err) {
-      console.error('Error adding test posts:', err);
-    }
+  const postsCollection = db.collection('posts');
+
+  try {
+    const now = moment(); // Current date and time in the specified time zone
+    const post1Timestamp = now.subtract(7, 'days'); // 7 days ago
+    const post2Timestamp = now.subtract(5, 'days'); // 5 days ago
+    const post3Timestamp = now.subtract(3, 'days'); // 3 days ago
+
+    const testPosts = [
+      { groupId: new ObjectId("651922d65535398850f5dbee") },
+      { groupId: new ObjectId("65191e089ace0b8414606b6a") },
+      { groupId: new ObjectId("6517d51dcbde365d3bfd0b02") },
+    ];
+
+    await postsCollection.insertMany(testPosts);
+    console.log('Test posts added:', testPosts);
+  } catch (err) {
+    console.error('Error adding test posts:', err);
   }
+}
 
 async function checkPosts(db) {
   const postsCollection = db.collection('posts');
-  
+
   try {
     const posts = await postsCollection.find({}).toArray();
-    console.log('Posts:', posts);
+    //console.log('Posts:', posts); // commented this because the output would be too long to read
   } catch (err) {
     console.error('Error checking posts:', err);
   }
@@ -59,8 +65,9 @@ async function performCRUDOperations(db) {
   const groupsCollection = db.collection('groups');
   const postsCollection = db.collection('posts');
 
-  // Create a new group
-  const newGroup = { name: 'Group A' };
+  //Only Create a new group operation is performed 
+  //Can add other CRUD operations as well
+  const newGroup = { name: 'Group B' };
   await groupsCollection.insertOne(newGroup);
   console.log('Group created:', newGroup);
 
@@ -70,55 +77,120 @@ async function performCRUDOperations(db) {
 }
 
 async function retrieveMostActiveGroups(db) {
-    const postsCollection = db.collection('posts');
-  
-    // Aggregation pipeline to retrieve most active groups based on recent post counts
-    const aggregationPipeline = [
-      {
-        $match: {
-          createdAt: { $gte: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000) } // Filter posts from the last 7 days (adjust the time frame as needed)
-        }
-      },
-      {
-        $group: {
-          _id: '$groupId',
-          totalRecentPosts: { $sum: 1 }
-        }
-      },
-      {
-        $sort: { totalRecentPosts: -1 }
-      },
-      {
-        $lookup: {
-          from: 'groups',
-          localField: '_id',
-          foreignField: '_id',
-          as: 'group'
-        }
-      },
-      {
-        $unwind: '$group'
-      },
-      {
-        $project: {
-          groupName: '$group.name',
-          totalRecentPosts: 1
-        }
+  const postsCollection = db.collection('posts');
+
+  // Can Adjust the time frame here 
+  const timeFrameInDays = 21; 
+
+  //Can Adjust the minimum post count required for a group to be considered active
+  const minPostCount = 5; 
+
+  // Aggregation pipeline to retrieve most active groups based on recent post counts
+  const aggregationPipeline = [
+    {
+      $match: {
+        createdAt: { $gte: moment().subtract(timeFrameInDays, 'days').toDate() } // Filter posts from the last 'timeFrameInDays' days
       }
-    ];
-  
-    try {
-      const activeGroups = await postsCollection.aggregate(aggregationPipeline).toArray();
-      console.log('Intermediate Result:', activeGroups); // Log intermediate result
-      if (activeGroups.length === 0) {
-        console.log('No active groups found within the specified time frame.');
-      } else {
-        console.log('Most active groups:', activeGroups);
+    },
+    {
+      $group: {
+        _id: '$groupId',
+        totalRecentPosts: { $sum: 1 }
       }
-    } catch (err) {
-      console.error('Error retrieving most active groups:', err);
+    },
+    {
+      $match: {
+        totalRecentPosts: { $gte: minPostCount } // Filter groups with at least 'minPostCount' posts
+      }
+    },
+    {
+      $sort: { totalRecentPosts: -1 }
+    },
+    {
+      $lookup: {
+        from: 'groups',
+        localField: '_id',
+        foreignField: '_id',
+        as: 'group'
+      }
+    },
+    {
+      $unwind: '$group'
+    },
+    {
+      $project: {
+        groupName: '$group.name',
+        totalRecentPosts: 1
+      }
     }
+  ];
+
+  try {
+    const activeGroups = await postsCollection.aggregate(aggregationPipeline).toArray();
+    console.log('Intermediate Result:', activeGroups); // Log intermediate result
+    if (activeGroups.length === 0) {
+      console.log('No active groups found within the specified criteria.');
+    } else {
+      console.log('Most active groups:', activeGroups);
+    }
+  } catch (err) {
+    console.error('Error retrieving most active groups:', err);
   }
+}
+
+connectToMongoDB();
+
+// Create an Express.js application
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware to open and close MongoDB connection
+app.use(async (req, res, next) => {
+  try {
+    req.db = await connectToMongoDB();
+    next();
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Defined API routes here 
+app.get('/api/posts', async (req, res) => {
+  const postsCollection = req.db.collection('posts');
+  try {
+    const posts = await postsCollection.find({}).toArray();
+    res.json(posts);
+  } catch (err) {
+    console.error('Error fetching posts:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the Express server
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Handle server shutdown
+process.on('SIGINT', () => {
+  closeMongoDBConnection().finally(() => {
+    console.log('Server and MongoDB connection closed.');
+    server.close(() => {
+      console.log('Express server closed.');
+      process.exit(0);
+    });
+  });
+});
+
+export async function closeMongoDBConnection() {
+  if (client.isConnected()) {
+    return client.close();
+  }
+}
   
 
 connectToMongoDB();
